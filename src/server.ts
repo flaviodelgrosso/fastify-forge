3079d6d10730bf3f4ae7ebb282d01e1d2d16bc07
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-01-08 11:37:52 +0100

    feat: add close-with-grace for graceful server shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
+import closeWithGrace from 'close-with-grace';
+
 import { buildApp } from './app';
 import env from './config/env.config';
 
 async function startServer() {
-  const server = buildApp({
+  const app = buildApp({
     logger: {
       level: env.log.level,
       redact: ['headers.authorization'],
@@ -10,34 +12,22 @@ async function startServer() {
     ignoreDuplicateSlashes: true,
   });
 
-  // Graceful shutdown
-  const signals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM'];
-  signals.forEach((signal) => {
-    process.on(signal, async () => {
-      try {
-        await server.close();
-        server.log.info(`Closed application on ${signal}`);
-        process.exit(0);
-      } catch (err) {
-        server.log.error(`Error closing application on ${signal}`, err);
-        process.exit(1);
-      }
-    });
+  closeWithGrace(async ({ signal, err }) => {
+    if (err) {
+      app.log.error({ err }, 'server closing with error');
+    } else {
+      app.log.info(`${signal} received, server closing`);
+    }
+    await app.close();
   });
 
   // Start server
   try {
-    await server.listen({ host: env.server.host, port: env.server.port });
+    await app.listen({ host: env.server.host, port: env.server.port });
   } catch (err) {
-    server.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 }
-
-// Handle unhandled rejections
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Rejection:', err);
-  process.exit(1);
-});
 
 startServer();
