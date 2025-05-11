42c4e1ec7bda1ea136b0bc4e116cfb79f12b787c
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-05-11 17:36:23 +0200

    chore: remove default under-pressure options from configuration

--- src/plugins/external/under-pressure.ts
+++ src/plugins/external/under-pressure.ts
@@ -1,12 +1,7 @@
 import FastifyUnderPressure, { type FastifyUnderPressureOptions } from '@fastify/under-pressure';
 
 export const autoConfig: FastifyUnderPressureOptions = {
-  maxEventLoopDelay: 1000,
-  maxHeapUsedBytes: 100_000_000,
-  maxRssBytes: 1_000_000_000,
-  maxEventLoopUtilization: 0.98,
-  message: 'The server is under pressure, retry later!',
-  retryAfter: 50,
+  // under-pressure options
 };
 
 export default FastifyUnderPressure;
