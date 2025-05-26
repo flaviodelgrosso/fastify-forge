88495a584791e56867a9179fb654979c7250398a
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-05-26 17:57:17 +0200

    refactor: add getDbDecorator function and remove module augmentation for db

--- src/plugins/internal/db.ts
+++ src/plugins/internal/db.ts
@@ -2,12 +2,6 @@ import type { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 import { db } from '../../db/index.ts';
 
-declare module 'fastify' {
-  interface FastifyInstance {
-    db: typeof db;
-  }
-}
-
 async function dbPlugin(fastify: FastifyInstance) {
   fastify.decorate('db', db);
   fastify.addHook('onClose', async () => {
