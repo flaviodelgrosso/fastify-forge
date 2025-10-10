93aa38224c6b6c23f7793d292fd8482dc4354ea4
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-10-10 13:14:25 +0200

    fix: update prepareEnv function to correctly reference the .env.example path

--- cli/utils.js
+++ cli/utils.js
@@ -67,8 +67,8 @@ export async function cleanUpRepo (projectDir) {
 }
 
 export async function prepareEnv (projectDir) {
-  const envPath = join(projectDir, '.env.example');
-  await copyFile(envPath, join(projectDir, '.env'));
+  const envPath = join(projectDir, "apps", "api", '.env.example');
+  await copyFile(envPath, join(projectDir, "apps", "api", '.env'));
 }
 
 async function getPackageJson (projectDir) {
